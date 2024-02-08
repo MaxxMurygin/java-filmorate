f3742a42f6aee1c12025cfbb3701f959b4ea826b
MaxxMurygin <maxx.murygin@gmail.com>
2024-02-08 19:18:58 +0700

    Спринт 9 - checkstyle

--- src/main/java/com/github/maxxmurygin/filmorate/validators/FilmValidator.java
+++ src/main/java/com/github/maxxmurygin/filmorate/validators/FilmValidator.java
@@ -6,15 +6,15 @@ import com.github.maxxmurygin.filmorate.model.Film;
 import java.time.LocalDate;
 
 public class FilmValidator implements Validator<Film> {
-
+    private static final int DESCRIPTION_LENGTH = 200;
+    private static final LocalDate MINIMUM_RELEASE_DATE = LocalDate.of(1895, 12, 28);
     @Override
     public void validate(Film film) {
-        int DESCRIPTION_LENGTH = 200;
-        LocalDate MINIMUM_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+
         if (film.getDescription().length() > DESCRIPTION_LENGTH) {
             throw new FilmValidationException("Слишком длинное описание");
         }
-        if (film.getReleaseDate().isBefore(MINIMUM_RELEASE_DATE)){
+        if (film.getReleaseDate().isBefore(MINIMUM_RELEASE_DATE)) {
             throw new FilmValidationException("Братья Люмьер еще не сделали это");
         }
     }
