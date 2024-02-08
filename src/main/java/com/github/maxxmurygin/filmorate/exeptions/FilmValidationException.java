a54fa8eb055afc3e0b3b13cf8894bfc527976db8
Maxx <maxx@aolc.ru>
2024-02-08 17:28:15 +0700

    Спринт 9 - checkstyle

--- src/main/java/com/github/maxxmurygin/filmorate/exeptions/FilmValidationException.java
+++ src/main/java/com/github/maxxmurygin/filmorate/exeptions/FilmValidationException.java
@@ -1,6 +1,6 @@
 package com.github.maxxmurygin.filmorate.exeptions;
 
-public class FilmValidationException extends RuntimeException{
+public class FilmValidationException extends RuntimeException {
     public FilmValidationException(String message) {
         super(message);
     }
--- src/main/java/com/github/maxxmurygin/filmorate/exeptions/UserValidationException.java
+++ src/main/java/com/github/maxxmurygin/filmorate/exeptions/UserValidationException.java
@@ -1,6 +1,6 @@
 package com.github.maxxmurygin.filmorate.exeptions;
 
-public class UserValidationException extends RuntimeException{
+public class UserValidationException extends RuntimeException {
     public UserValidationException(String message) {
         super(message);
     }
