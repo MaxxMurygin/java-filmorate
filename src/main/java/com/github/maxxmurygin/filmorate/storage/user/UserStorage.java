c78acec4921b3a1c737a5db7f8d9060b44241cdf
MaxxMurygin <maxx.murygin@gmail.com>
2024-02-28 18:43:00 +0700

    Sprint 10 - 1st pull request

--- src/main/java/com/github/maxxmurygin/filmorate/storage/user/UserStorage.java
+++ src/main/java/com/github/maxxmurygin/filmorate/storage/user/UserStorage.java
@@ -5,12 +5,17 @@ import com.github.maxxmurygin.filmorate.model.User;
 import java.util.Collection;
 
 public interface UserStorage {
-
     User create(User user);
+
     User update(User user);
+
     User findByEmail(String email);
+
     User findById(Integer id);
+
     Collection<User> findAll();
+
     User addFriend(Integer userId, Integer friendId);
+
     User removeFriend(Integer userId, Integer friendId);
 }
